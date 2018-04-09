00de9b10358fdef4d0bce00691d2fbe912435bb2
Mike K <m.kermes89@gmail.com>
2018-04-09 16:06:25 -0700

    tried forming some objects with assigned properties, not working yet

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
-var 
+var locations = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'],
+
+var minCust = [23, 3, 11, 20, 2],
+
+var maxCust = [65, 24, 38, 38, 16],
+
+var cookieSale = [6.3, 1.2, 3.7, 2.3, 4.6],
+
+
+var cookieStore = {
+  storeLocation:'1st and Pike',
+  minCust:[0];
+  maxCust:[0];
+  cookieSale[0];
+}
+
+console.log(cookieStore);
