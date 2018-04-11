1f442b36f4a77dd3de57e32d6adfc6aa9d568a50
Mike K <m.kermes89@gmail.com>
2018-04-11 16:40:25 -0700

    added form inputs for constructor properties

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var allStores = [];
 var storeTable = document.getElementById('store');
-var storeHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+var storeHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
 
 //add property to object for cookies sold each hour and something for //total cookies. eventually print out total cookies as adding hourly //sales array position, similarly for other totals
 
